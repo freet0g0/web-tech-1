70a5bc9b44c914f8e656ebd7f588b11ca0db14cb
CchanN <123476850+CchanN@users.noreply.github.com>
2023-01-25 12:03:46 +0800

    web-tech
    trial

---
+++ App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div class="container">
+          <h2>Hello World!</h2>    
+          <h4>Christian Conde</h4>
+      </div>
+    </div>
+    
+  );
+}
+
+export default App;
